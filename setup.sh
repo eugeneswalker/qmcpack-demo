d7d7459845fdca317f1f3ab2dff998fb1e91c546
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-02-13 09:26:40 -0800

    update workflow

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-SPACK_REF=9d8b8553f1ad95f26be2a03fbf94c1fdec257792
+#SPACK_REF=9d8b8553f1ad95f26be2a03fbf94c1fdec257792
+SPACK_REF=851490bd5424f327edbde44d6d78a9cfe867df9a # 2-13-2021
 
 if [ ! -d spack ] ; then
   echo Cloning Spack@${SPACK_REF}
@@ -14,10 +15,13 @@ fi
 
 echo Configuring Spack to use E4S Build Cache
 spack mirror rm E4S >/dev/null 2>&1 || :
-spack mirror add E4S https://cache.e4s.io
+spack mirror add E4S https://cache.e4s.io/qmcpack
 [ ! -f e4s.pub ] && wget https://oaciss.uoregon.edu/e4s/e4s.pub
 spack gpg trust e4s.pub
 
 spack -e . concretize -f | tee qmcpack.dag
 
-time spack -e . install --cache-only
+time spack -e . install \
+	--cache-only \
+	--include-build-deps \
+	--only dependencies
